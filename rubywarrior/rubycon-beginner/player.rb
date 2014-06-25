9e5393ea72552781246e451511c3cfce08ea4758
Fernando Moreno  Oviedo <ferunic@gmail.com>
2014-06-25 23:13:02 +0200

    level 4 complete

--- rubywarrior/rubycon-beginner/player.rb
+++ rubywarrior/rubycon-beginner/player.rb
@@ -1,9 +1,14 @@
 class Player
+	@health = 20
   def play_turn(warrior)
-		if warrior.feel.empty? 
-			warrior.health >= 20? warrior.walk! : warrior.rest!
+		if warrior.feel.empty? 			
+			(warrior.health >= 20 or getting_hurt(warrior))?  warrior.walk! :  warrior.rest!
 		else
 			warrior.attack!
 		end
+		@health= warrior.health		
+  end
+  def getting_hurt(warrior)
+	return warrior.health < @health
   end
 end
