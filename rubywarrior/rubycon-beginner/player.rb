ecbd7a1f2a6e283f0eb0bb6d6be6516a698840e6
Fernando Moreno  Oviedo <ferunic@gmail.com>
2014-06-26 10:30:51 +0200

    Level 6 Complete

--- rubywarrior/rubycon-beginner/player.rb
+++ rubywarrior/rubycon-beginner/player.rb
@@ -1,14 +1,34 @@
 class Player
 	@health = 20
+
   def play_turn(warrior)
-		if warrior.feel.empty? 			
-			(warrior.health >= 20 or getting_hurt(warrior))?  warrior.walk! :  warrior.rest!
+  
+  case warrior.health
+  when 0..9
+	#must restore soon, go backwards?
+	if warrior.feel(:backward).empty? 
+		getting_hurt(warrior)? warrior.walk!(:backward) : warrior.rest!
+	elsif  warrior.feel(:backward).captive
+		warrior.rescue!(:backwards)
+	else
+		getting_hurt(warrior)? warrior.walk! : warrior.rest!
+	end
+  when 10..20
+ 		if warrior.feel.empty? 			
+			getting_hurt(warrior)?  warrior.walk! : warrior.health < 20? warrior.rest! : warrior.walk!
 		else
-			warrior.attack!
+			warrior.feel.captive?? warrior.rescue! : warrior.attack!
 		end
-		@health= warrior.health		
+		
+	end
+  @health= warrior.health		
   end
+  
   def getting_hurt(warrior)
-	return warrior.health < @health
+	if @health.nil?  
+		return false 
+	else
+		return warrior.health < @health
+	end
   end
 end
